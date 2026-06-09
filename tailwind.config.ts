5fa1c5df27de917ea82105635112c39f3ad043df
santani423 <santani423@gmail.com>
2026-06-10 03:31:27 +0700

    ok

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,18 +6,18 @@ module.exports = {
   ],
   theme: {
     screens: {
-      xxxs: '340px',
+      xxxs: '340px', // galaxy fold ok
       xxs: '360px', //samsung galaxy s20 412 ok
       xs: '375px', // iphone se 375x667 ok
       s: '390px', // iphone 12 pro 390x844 ok
       s2: '392px', //   pcoco 392x734 ok
       iphone: '412px', // iphone xr 414x896,iphone 14 pro max 430x932 ok
       mobile: '540px',  // surface duo 540x720 ok
-      sm: '640px', //  huawei  686x977 potret ok
-      md: '768px', // ipad min 768x1024 ok
+      sm: '640px', //  huawei  686x977 potret  
+      md: '768px', // ipad min 768x1024  ok
       md2: '810px', // ipad air 820x1180 ok
       md3: '850px', // assus zendb 853 x 1280 ok
-      tb: '900px', //  surface pro 7 912x1368 ok
+      tb: '900px', //  surface pro 7 912x1368 OK
       lg: '1024px', // ipad pro 1024x1366 ok
       lg2: '1090px', // huawei google chrome 1097x566 ok
       lg3: '1098px', // huawei  1092x566 ok
